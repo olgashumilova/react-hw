6e6e3898417f3f4d4e277ced07bdf26ef7aef9c1
olgashumilova <volga_shumilova@mail.ru>
2021-02-28 21:58:54 +0300

    feat: add tasks

--- reacthw/src/App.js
+++ reacthw/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-import Ex1 from "./components/Ex1/ex1";
-import Ex2 from "./components/Ex2/ex2";
+import Ex1 from "./components/Ex1";
+import Ex2 from "./components/Ex2";
+import Square from './components/Ex3'
+import Ex4 from './components/Ex4'
 
 function App() {
   return (
     <div className="App">
 
       <Ex1 />
-      <Ex2 />
+      <Ex2 text = "Lorem ipsum dolor"/>
+
+      <Square label='Big square' cssClass='big-square'/>
+      <Square label='Medium square' cssClass='medium-square'/>
+      <Square label='Small square' cssClass='small-square'/>
+
+      <Ex4 />
 
     </div>
   );
