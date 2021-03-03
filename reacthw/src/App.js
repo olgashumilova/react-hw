aa2b2d3ec51fb05281de39fa7f150328fcd1a1f2
olgashumilova <volga_shumilova@mail.ru>
2021-03-03 14:01:49 +0300

    fix: delete Ex4

--- reacthw/src/App.js
+++ reacthw/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Ex1 from "./components/Ex1";
 import Ex2 from "./components/Ex2";
 import Square from './components/Ex3'
-import Ex4 from './components/Ex4'
 
 function App() {
   return (
@@ -14,8 +13,6 @@ function App() {
       <Square label='Big square' cssClass='big-square'/>
       <Square label='Medium square' cssClass='medium-square'/>
       <Square label='Small square' cssClass='small-square'/>
-
-      <Ex4 />
 
     </div>
   );
