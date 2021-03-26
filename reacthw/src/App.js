cc1517346253b062ebac77f01365de144c0a16c1
olgashumilova <volga_shumilova@mail.ru>
2021-03-26 20:11:42 +0300

    feat: add Ex1, Ex2, Ex3

--- reacthw/src/App.js
+++ reacthw/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Ex1 from './components/Ex1'
+import Ex2 from './components/Ex2'
+import Ex3 from './components/Ex3'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Ex1 />
+
+      <Ex2 />
+
+      <Ex3 label='Кнопка 1' clickFunction = {() => console.log('Была нажата кнопка 1')}/>
+      <Ex3 label='Кнопка 2' clickFunction = {() => alert('Была нажата кнопка 2')}/>
+      <Ex3 label='Кнопка с параметром' clickFunction = {(param) => console.log(`${param}'Кнопка с параметром'`)}/>
+
     </div>
   );
 }
